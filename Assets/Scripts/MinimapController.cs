a844d8aea3e5345cc70dce238daf3a71349d9e15
sheenashea <3061851662@qq.com>
2024-12-15 08:40:03 +0800

    加上小地图以及m点击事件

---
+++ Assets/Scripts/MinimapController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class MinimapController : MonoBehaviour {
+    public Camera minicamera;
+    public Transform player;
+    public Transform miniplayerIcon;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        minicamera.transform.position = new Vector3(player.position.x, minicamera.transform.position.y, player.position.z);
+        miniplayerIcon.eulerAngles = new Vector3(0, 0, -player.eulerAngles.y);
+    }
+}
